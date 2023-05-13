230f4a84e3aa6005680a76b1ee3085efc197d5c6
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-05-13 18:55:31 +0200

    Create component List and add ready styles do /src/styles

---
+++ src/components/List/List.js
@@ -0,0 +1,23 @@
+const List = () => {
+  return (
+    <header>
+      <h2>Things to do<span>soon!</span></h2>
+      <p>Interesting things I want to check out</p>
+      <section>
+        <article>
+          <h2>Books</h2>
+        </article>
+
+        <article>
+          <h2>Movies</h2>
+        </article>
+
+        <article>
+          <h2>Games</h2>
+        </article>
+      </section>
+    </header>
+  );
+};
+
+export default List;
