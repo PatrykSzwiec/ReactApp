d424f64733a9aad4f6a265502eeb786e495639a7
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-05-13 19:19:42 +0200

    Add styles for components and implement it at js code

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,22 +1,24 @@
+import styles from './List.module.scss';
+
 const List = () => {
   return (
-    <header>
-      <h2>Things to do<span>soon!</span></h2>
-      <p>Interesting things I want to check out</p>
-      <section>
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Things to do<span className={styles.span}>soon!</span></h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
         <article>
-          <h2>Books</h2>
+          <h2 className="title">Books</h2>
         </article>
-
         <article>
-          <h2>Movies</h2>
+          <h2 className="title">Movies</h2>
         </article>
-
         <article>
-          <h2>Games</h2>
+          <h2 className="title">Games</h2>
         </article>
       </section>
-    </header>
+    </div>
   );
 };
 
