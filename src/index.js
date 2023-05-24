8cf35b8622c768c8374637040f9876bd0671b799
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-05-24 20:12:35 +0200

    Modify List and Column component to use Redux storage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
@@ -6,4 +9,10 @@ import 'font-awesome/css/font-awesome.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
- root.render(<App tab="home"/>);
+ root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+  );
