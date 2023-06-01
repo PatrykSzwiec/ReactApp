b838fdadc3047f8fb8f2c664ab1f95e8dacc0d1b
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-06-01 23:35:46 +0200

    Add About and Favorite components, add browser router for index.js and modift app.js with routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import Hero from './components/Hero/Hero';
-import SearchForm from './components/SearchForm/SearchForm';
-import List from './components/List/List';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
+import Favorite from './components/Favorite/Favorite';
+import Home from './components/Home/Home';
+
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
       </Container>
     </main>
   );
