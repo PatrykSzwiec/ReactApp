307dd4fcaea82d5e1bce05d4384c2927717dacb2
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-06-01 23:45:40 +0200

    Add NotFound page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import About from './components/About/About';
 import Favorite from './components/Favorite/Favorite';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound.js';
 
 import { Routes, Route } from 'react-router-dom';
 
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
     </main>
